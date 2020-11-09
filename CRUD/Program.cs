b2366eabfd5d8777715c39ad03799283d7e1a1a0
Sebastian Tomczak <sebas.tomczak@gmail.com>
2020-11-09 16:44:20 +0100

    Some moves in solution + Creating methods for CRUD. Adding model class.

--- CRUD/Program.cs
+++ CRUD/Program.cs
@@ -9,40 +9,9 @@ namespace CRUD
     {
         static void Main(string[] args)
         {
-            string sqlCreate;
-
-            using (StreamReader streamReader = new StreamReader(".\\Scripts\\CreateDb.sql", Encoding.UTF8))
-            {
-                sqlCreate = streamReader.ReadToEnd();
-            }
-
             DbConnection.OpenConnection(DbConnection.GetConnection());
 
-            using (var command = new MySqlCommand(sqlCreate, DbConnection.GetConnection()))
-            {
-                command.ExecuteNonQuery();
-            }
-
-            using (var command = new MySqlCommand("INSERT INTO PFSwChO.dane(name, surname, age)VALUES(\"val1\", \"val2\", \"1\")", DbConnection.GetConnection()))
-            {
-                command.ExecuteNonQuery();
-            }
-
-            using (var command = new MySqlCommand("UPDATE PFSwChO.dane SET name=\"patyk\" WHERE age=\"1\"", DbConnection.GetConnection()))
-            {
-                command.ExecuteNonQuery();
-            }
-
-            using (var command = new MySqlCommand("SELECT name, surname, age FROM PFSwChO.dane", DbConnection.GetConnection()))
-            {
-                using (MySqlDataReader rdr = command.ExecuteReader())
-                {
-                    while (rdr.Read())
-                    {
-                        Console.WriteLine($"{rdr["name"]} - {rdr["surname"]} - {rdr["age"]}"); //TODO Reader jest jak komunista, trzeba to naprawić
-                    }
-                }
-            }
+            //Konsola - UI
         }
     }
 }
